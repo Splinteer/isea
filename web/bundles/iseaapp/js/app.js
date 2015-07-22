c3bab70ab4435baf5b493e672396cf181fdacb88
Marvin Sanchez <sanchezmarvin38@gmail.com>
2015-07-22 14:19:30 +0200

    Revert "Minor changes"
    This reverts commit 1fd55208cf23b433beaa7d6e56148a2b543bb663.

--- web/bundles/iseaapp/js/app.js
+++ web/bundles/iseaapp/js/app.js
@@ -1,16 +1,49 @@
-$(document).ready(function(){
-    //Menu
-    $("#trigger-overlay").on('click', function(){
-        $('.overlay').addClass('open');
-    });
-    $(".overlay-close").on('click', function(){
-        $('.overlay').removeClass('open');
-    });
+(function() {
+    var triggerBttn = document.getElementById( 'trigger-overlay' ),
+        overlay = document.querySelector( 'div.overlay' ),
+        closeBttn = overlay.querySelector( 'button.overlay-close' );
+        transEndEventNames = {
+            'WebkitTransition': 'webkitTransitionEnd',
+            'MozTransition': 'transitionend',
+            'OTransition': 'oTransitionEnd',
+            'msTransition': 'MSTransitionEnd',
+            'transition': 'transitionend'
+        },
+        transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
+        support = { transitions : Modernizr.csstransitions };
 
+    function toggleOverlay() {
+        if( classie.has( overlay, 'open' ) ) {
+            classie.remove( overlay, 'open' );
+            classie.add( overlay, 'close' );
+            var onEndTransitionFn = function( ev ) {
+                if( support.transitions ) {
+                    if( ev.propertyName !== 'visibility' ) return;
+                    this.removeEventListener( transEndEventName, onEndTransitionFn );
+                }
+                classie.remove( overlay, 'close' );
+            };
+            if( support.transitions ) {
+                overlay.addEventListener( transEndEventName, onEndTransitionFn );
+            }
+            else {
+                onEndTransitionFn();
+            }
+        }
+        else if( !classie.has( overlay, 'close' ) ) {
+            classie.add( overlay, 'open' );
+        }
+    }
+
+    triggerBttn.addEventListener( 'click', toggleOverlay );
+    closeBttn.addEventListener( 'click', toggleOverlay );
     $('#rapidAccess').hide();
     $('#rapidAccessButton').on('click', function(){
         $('#rapidAccess').fadeIn();
     });
+})();
+
+$(document).ready(function(){
     function scrollToID(id, speed){
         var offSet = 50;
         var targetOffset = $(id).offset().top - offSet;
